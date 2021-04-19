99b8e8fd0e762b5ad57ccde02dfba2c068d9629c
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-04-18 20:01:25 -0600

    created slider controlled brightness script; applied to on light

---
+++ Scripts/PhysicsSliderControlledBrightness.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhysicsSliderControlledBrightness : MonoBehaviour
+{
+    public Light light;
+    public float minIntensity;
+    public float maxIntensity;
+    public Slider slider;
+
+    public void OnSliderValueChanged()
+    {
+        light.intensity = Mathf.Lerp(minIntensity, maxIntensity, slider.value);
+    }
+}
